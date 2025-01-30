9f2e16917ef144a641700780a642da53dda9e312
Sergey Smetannikov <sergenevsky@gmail.com>
2025-01-30 16:44:19 +0700

    Initial base apps structure.

---
+++ documentor-repo/apps/backend-e2e/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'backend-e2e',
+  preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/backend-e2e',
+};
